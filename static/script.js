8afadddebe4fab3bc314234c8bd756cdbffbe039
Akhil Krishna <akhilmavannoor@gmail.com>
2024-04-12 06:07:24 +0530

    Website UI Changed entirely : with deleting all unnecessary files

---
+++ static/script.js
@@ -0,0 +1,26 @@
+const menus = document.querySelector("nav ul");
+const menusBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+let closed=1
+
+menusBtn.addEventListener("click", function () {
+    if (closed==1){
+        menus.classList.add("display");
+        closed=0
+    }
+    else{
+        menus.classList.remove("display");
+        closed=1
+    }
+
+});
+
+closeBtn.addEventListener("click", function () {
+  menus.classList.remove("display");
+});
+
+function closer(){
+    menus.classList.remove("display");
+}
+
